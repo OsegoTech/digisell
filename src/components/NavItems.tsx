f11ab6e976b2b9fce1b2354670f770e81cf210be
Omollo Oganda <cliffosego2000@gmail.com>
2024-03-02 19:21:06 +0300

    feature:core Design a beautiful Navbar

---
+++ src/components/NavItems.tsx
@@ -0,0 +1,49 @@
+"use client"
+
+import { PRODUCT_CATEGORIES } from "@/config"
+import { useEffect, useRef, useState } from "react"
+import NavItem from "./NavItem"
+import { useOnClickOutside } from "@/hooks/use-onclick-outside"
+
+const NavItems = () => {
+const [activeIndex, setActiveIndex] = useState<null | Number>(null)
+
+useEffect(() => {
+    const handler = (e: KeyboardEvent) => {
+        if(e.key === "Escape"){
+            setActiveIndex(null)
+        }
+    }
+    document.addEventListener("keydown", handler)
+    return () => {
+        document.removeEventListener("keydown", handler)
+    }
+}, [])
+
+const isAnyOpen = activeIndex !== null
+
+const navRef = useRef<HTMLDivElement |null> (null)
+
+useOnClickOutside(navRef, ()=> setActiveIndex(null))
+
+return (
+<div className="flex gap-4 h-full" ref={navRef}>
+    {PRODUCT_CATEGORIES.map((category, i) => {
+        const handleOpen = () => {
+            if(activeIndex === 1){
+                setActiveIndex(null)
+            } else {
+                setActiveIndex(i)
+            }
+        }
+
+        const isOpen =  i === activeIndex
+        return (
+            <NavItem category={category} handleOpen={handleOpen} isOpen={isOpen} key={category.value} isAnyOpen={isAnyOpen}/>
+        )
+    })}
+</div>
+)
+}
+
+export default NavItems
